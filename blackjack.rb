83b6423359f6b2b7f476187bde6876b8934c4fd4
JuliaLes <JCLes92@gmail.com>
2016-05-15 08:54:15 -0400

    updating blackjack hw-still in progress

--- blackjack.rb
+++ blackjack.rb
@@ -1,72 +1,71 @@
 
-# Blackjack program as of 5/14/2016- in progress. Much work needed. 
+# Blackjack program as of 5/15/2016- in progress. Much work needed. 
+
+    
+class Player
+
+    def initialize
+        puts "What is the player's name? "
+        @name = gets.chomp
+        
+        puts "What is #{@name}'s bank roll value? "
+        @bankroll = gets.chomp.to_i
+        
+        puts "How much is #{@name} wagering for this game?"
+        @wager = gets.chomp.to_i
+        
+        @hand = []
+        puts "The player #{@name} is ready to begin."
+        
+    end
+
+    
+    def draws_card
+        @hand << [$deck.pop]
+        puts "A card has been drawn"
+    end
+    
+    
+    def hand_sum
+        sum = @hand.inject(:+).to_i
+    end
+    
+    def display_full_hand
+        puts "#{@name}'s hand is' "+ @hand.join(', ')
+    end
+             
+end
+
+
+class Dealer < Player
+
+
+    def initialize
+        puts "What is the dealer's name?"
+        @name = gets.chomp
+        @hand = []
+        puts "The dealer #{@name} is ready to begin."
+    end
+      
+    def display_part_hand
+        puts "The dealer #{@name} is holding a card with the value #{@hand[0].to_s}. The other card is concealed."
+    end  
+
+end
+
 
 class Blackjack    
 
-    def initialize
-        puts "Welcome to Blackjack. Let's get started!"
-    end
+
+    puts "\nWelcome to Blackjack. Let's get started!\n"
+        
 
     class Deck
-        def intitialize
-            puts "creating deck"
-            @@deck = (((1..9).to_a*4).concat [10]*16).shuffle!
-        end
-    end 
-
-
-    
-    class Dealer
-        def initialize
-            puts "What is the dealer's name?"
-            @name = gets.chomp
-            @hand = []
-            puts "The dealer #{@name} is ready to begin."
-        end
-        
-        def draws_card
-            @hand << [@@deck.pop]
-        end
-        
-        
-        def hand_sum
-            sum = @hand.inject(:+).to_i
-        end
-        
-        
-        def display_hand
-            puts "The dealer #{@name} is holding a card with the value #{@hand[0].to_s}. The other card is concealed."
-        end
-                 
-    end
-    
-    
-    class Player < Dealer
-        def initialize
-            puts "What is the player's name? "
-            @name = gets.chomp
-            
-            puts "What is #{@name}'s bank roll value? "
-            @bankroll = gets.chomp.to_i
-            
-            puts "How much is #{@name} wagering for this game?"
-            @wager = gets.chomp.to_i
-            
-            @hand = []
-            puts "The player #{@name} is ready to begin."
-            
-        end
-            
-        def display_hand
-            puts "The player #{@name} is holding the following: #{@hand.join(',')}."
-        end
-
+        puts "\nA shuffled deck of cards is ready to use.\n"
+        $deck = (((1..9).to_a*4).concat [10]*16).shuffle!
     end
 
-    
-
-    # Now we can begin the game
-    
+    deck = Deck.new
 
     dealer = Dealer.new
     player = Player.new
@@ -77,22 +76,22 @@ class Blackjack
     player.draws_card
     
     puts " "
-    dealer.display_hand
+    dealer.display_part_hand
     puts " "
-    player.display_hand
+    player.display_full_hand
     puts " "
     
     
     #The player is making decisions
     
     while player.hand_sum < 21
-        puts "Player- would you like to hit or stay?"
+        puts "\nPlayer- would you like to hit or stay?\n"
         player_choice = gets.chomp.downcase
         if player_choice == "hit"
             player.draws_card
-            player.display_hand
+            player.display_full_hand
         elsif player_choice == "stay"
-            player.display_hand 
+            player.display_full_hand 
             break
         else 
             puts 'Please type "hit" or "stay."'
@@ -100,28 +99,26 @@ class Blackjack
     end
     
     if player.hand_sum > 21
-        puts "BUST! The dealer wins!"
+        puts "\nBUST! The dealer wins!\n"
         exit
     elsif player.hand_sum == 21
-        puts "BLACKJACK! The player wins!"
+        puts "\nBLACKJACK! The player wins!\n"
         exit
     else 
-        puts "Now it is the dealer's turn."
+        puts "\nNow it is the dealer's turn.\n"
     end
-        
-        
         
     
     # The dealer is making decisions    
     
     while dealer.hand_sum < 21
-        puts "Dealer- would you like to hit or stay?"
-        dealer_choice = gets.chomp.downcase
-        if dealer_choice == "hit"
+        if dealer_hand_sum < 17
+            puts "\nThe dealer has decided to hit.\n"
             dealer.draws_card
-            dealer.display_hand
-        elsif dealer_choice == "stay"
-            dealer.display_hand 
+            dealer.display_full_hand
+        elsif dealer_hand_sum >= 17
+            puts "\nThe dealer has decided to stay.\n"
+            dealer.display_full_hand 
             break
         else 
             puts 'Please type "hit" or "stay."'
@@ -130,10 +127,10 @@ class Blackjack
         
         
     if dealer.hand_sum > 21
-        puts "BUST! The player wins!"
+        puts "\nBUST! The player wins!\n"
         exit
     elsif player.hand_sum == 21
-        puts "BLACKJACK! The dealer wins!"
+        puts "\nBLACKJACK! The dealer wins!\n"
         exit
     end
     
@@ -141,13 +138,13 @@ class Blackjack
     # Final comparison
     
     if dealer.hand_sum == player.hand_sum
-        puts "It's a tie!"
+        puts "\nIt's a tie!\n"
         exit
     elsif dealer.hand_sum > player.hand_sum
-        puts "The dealer wins!"
+        puts "\nThe dealer wins!\n"
         exit
     else
-        puts "The player wins!"
+        puts "\nThe player wins!\n"
         exit
     end
 end
